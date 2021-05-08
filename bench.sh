854a7b067e3a2d5eac33f8b03137d6ca8a243eb9
avi <hi@avi.im>
2021-05-08 14:32:42 +0530

    verify data at the end

--- bench.sh
+++ bench.sh
@@ -6,8 +6,8 @@ rm -rf naive.db naive.db-shm naive.db-wal
 echo "$(date)" "[PYTHON] running naive.py (10_000_000) inserts"
 /usr/bin/time python3 naive.py
 # lets verify the data exists
-if [[ $(sqlite3 naive.db  "select count(*) from user";) == 10000000 ]]; then
-  echo "data verified"
+if [[ $(sqlite3 naive.db  "select count(*) from user";) != 10000000 ]]; then
+  echo "data verification failed"
 fi
 
 # benching naive batched
@@ -15,8 +15,8 @@ rm -rf naive_batched.db naive_batched.db-shm naive_batched.db-wal
 echo
 echo "$(date)" "[PYTHON] running naive_batched.py (10_000_000) inserts"
 /usr/bin/time python3 naive_batched.py
-if [[ $(sqlite3 naive_batched.db  "select count(*) from user";) == 10000000 ]]; then
-  echo "data verified"
+if [[ $(sqlite3 naive_batched.db  "select count(*) from user";) != 10000000 ]]; then
+  echo "data verification failed"
 fi
 
 # benching sqlite3 optimized
