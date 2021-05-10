009694f3dc1e6dd9d6caa6f57a8d5fd9432f4f83
avi <hi@avi.im>
2021-05-10 22:23:54 +0530

    fix basic_batched

--- src/bin/basic_batched.rs
+++ src/bin/basic_batched.rs
@@ -1,4 +1,4 @@
-use rusqlite::{params, Connection, Transaction};
+use rusqlite::{Connection, ToSql, Transaction};
 
 mod common;
 
@@ -9,26 +9,53 @@ fn faker_wrapper(mut conn: Connection, count: i64) {
 }
 
 fn faker(tx: &Transaction, count: i64) {
-    let mut stmt_with_area = tx
-        .prepare_cached("INSERT INTO user VALUES (NULL, ?, ?, ?)")
-        .unwrap();
-    let mut stmt = tx
-        .prepare_cached("INSERT INTO user VALUES (NULL, NULL, ?, ?)")
-        .unwrap();
-    let min_batch_size = 1_000_000;
+    // that is, we will batch 50 inserts of rows at once
+    let min_batch_size: i64 = 50;
+    if count < min_batch_size {
+        panic!("count cant be less than min batch size");
+    }
+
+    // jeez, refactor this!
+    let mut with_area_params = " (NULL, ?, ?, ?),".repeat(min_batch_size as usize);
+    with_area_params.pop();
+    let with_area_params = with_area_params.as_str();
+    let mut without_area_params = " (NULL, NULL, ?, ?),".repeat(min_batch_size as usize);
+    without_area_params.pop();
+    let without_area_params = without_area_params.as_str();
+    let st1 = format!("INSERT INTO user VALUES {}", with_area_params);
+    let st2 = format!("INSERT INTO user VALUES {}", without_area_params);
+
+    let mut stmt_with_area = tx.prepare_cached(st1.as_str()).unwrap();
+    let mut stmt = tx.prepare_cached(st2.as_str()).unwrap();
     for _ in 0..(count / min_batch_size) {
         let with_area = common::get_random_bool();
         let age = common::get_random_age();
         let is_active = common::get_random_active();
-        for _ in 0..min_batch_size {
-            if with_area {
+        let mut param_values: Vec<_> = Vec::new();
+        if with_area {
+            // lets prepare the batch
+            let mut vector = Vec::<(String, i8, i8)>::new();
+            for _ in 0..min_batch_size {
                 let area_code = common::get_random_area_code();
-                stmt_with_area
-                    .execute(params![area_code, age, is_active])
-                    .unwrap();
-            } else {
-                stmt.execute(params![age, is_active]).unwrap();
+                vector.push((area_code, age, is_active));
             }
+            for batch in vector.iter() {
+                param_values.push(&batch.0 as &dyn ToSql);
+                param_values.push(&batch.1 as &dyn ToSql);
+                param_values.push(&batch.2 as &dyn ToSql);
+            }
+            stmt_with_area.execute(&*param_values).unwrap();
+        } else {
+            // lets prepare the batch
+            let mut vector = Vec::<(i8, i8)>::new();
+            for _ in 0..min_batch_size {
+                vector.push((age, is_active));
+            }
+            for batch in vector.iter() {
+                param_values.push(&batch.0 as &dyn ToSql);
+                param_values.push(&batch.1 as &dyn ToSql);
+            }
+            stmt.execute(&*param_values).unwrap();
         }
     }
 }
