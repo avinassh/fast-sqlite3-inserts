37a11df8915ebe29ede78e9549578ab40b904272
Nemo <me@captnemo.in>
2021-07-13 23:40:22 +0530

    Add note about why using range is faster

--- commons.py
+++ commons.py
@@ -19,6 +19,8 @@ def create_table(con: sqlite3.Connection):
 def get_random_area_code() -> str:
     return str(random.randint(100000, 999999))
 
+# Slightly faster. The doc for random.sample mentions:
+# To choose a sample from a range of integers, use a range() object as an argument. This is especially fast and space efficient for sampling from a large population: sample(range(10000000), k=60).
 def get_random_age() -> int:
     return random.choice(range(5,16,5))
 
