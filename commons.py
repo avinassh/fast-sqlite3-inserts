199007d12cee9c8550f354080499e661506e335f
Nemo <me@captnemo.in>
2021-07-14 20:36:12 +0530

    Speed up the commons functions (#2)
    * Speed up the commons functions
    
    My 10M (sqlite3_opt_batched) run goes from 42s to 26s.
    
    Switching to https://docs.python.org/3/library/os.html#os.urandom would
    make this even faster, but I don't think that's the point of the
    exercise so leaving it at this. I think it's still nice to have because
    it reduces the time by a lot and makes it _faster for me to benchmark_
    
    * Add note about why using range is faster

--- commons.py
+++ commons.py
@@ -17,16 +17,15 @@ def create_table(con: sqlite3.Connection):
 
 
 def get_random_area_code() -> str:
-    return ''.join([F"{random.randint(0, 9)}" for _ in range(0, 6)])
+    return str(random.randint(100000, 999999))
 
-
+# Slightly faster. The doc for random.sample mentions:
+# To choose a sample from a range of integers, use a range() object as an argument. This is especially fast and space efficient for sampling from a large population: sample(range(10000000), k=60).
 def get_random_age() -> int:
-    return random.choice([5, 10, 15])
-
+    return random.choice(range(5,16,5))
 
 def get_random_active() -> int:
-    return 1 if get_random_bool() else 0
-
+    return random.getrandbits(1)
 
 def get_random_bool() -> bool:
     return bool(random.getrandbits(1))
