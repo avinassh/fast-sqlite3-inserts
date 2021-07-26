3a30d6688ee07d7d7eb3d7bf866820f984a10024
Jiahao XU <Jiahao_XU@outlook.com>
2021-07-26 21:14:30 +1000

    Set SQLITE3_LIB_DIR for dep crate rusqlite
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- cargo_build.sh
+++ cargo_build.sh
@@ -4,7 +4,10 @@ base="$(dirname `realpath $0`)"
 
 cd "$base"
 
-export PKG_CONFIG_PATH="$base/sqlite3"
+export SQLITE3_LIB_DIR="$base/sqlite3"
+export PKG_CONFIG_PATH="$SQLITE3_LIB_DIR/pkgconfig"
 export SQLITE3_STATIC=1
+
+pkg-config sqlite3 --cflags --libs
 
 exec cargo build --release $@
