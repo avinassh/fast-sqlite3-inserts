e6b3475b96a915a5270f161fc206f200535eed17
Jiahao XU <Jiahao_XU@outlook.com>
2021-07-27 14:01:32 +1000

    Fix env required to use custom libsqlite3.a in cargo_build.sh
    SQLITE3_INCLUDE_DIR is necessary since libsqlite3-sys does not use sqlite3.pc
    found using `SQLITE3_LIB_DIR` for locating the header.
    
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- cargo_build.sh
+++ cargo_build.sh
@@ -4,10 +4,15 @@ base="$(dirname `realpath $0`)"
 
 cd "$base"
 
+# SQLITE3_LIB_DIR is necessary the directory of statically linked library.
+# Otherwise, libsqlite3-sys would just use the global dynamic library
 export SQLITE3_LIB_DIR="$base/sqlite3"
-export PKG_CONFIG_PATH="$SQLITE3_LIB_DIR/pkgconfig"
+# SQLITE3_STATIC is necessary to statically linked with libsqlite3.a
 export SQLITE3_STATIC=1
+# SQLITE3_INCLUDE_DIR is necessary since libsqlite3-sys does not use sqlite3.pc
+# found using `SQLITE3_LIB_DIR` for locating the header.
+export SQLITE3_INCLUDE_DIR="$SQLITE3_LIB_DIR"
 
-pkg-config sqlite3 --cflags --libs
+export PKG_CONFIG_PATH="$SQLITE3_LIB_DIR/pkgconfig"
 
-exec cargo build --release $@
+exec cargo build --release -vv $@
